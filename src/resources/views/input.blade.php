e7e09497b08c251d6b6a49dc4a915abdc0d0d14e
Thiemo Kunze <kunze@wangaz.com>
2016-06-22 11:52:00 +0200

    adding semantic ui + proper value-attr handling

--- src/resources/views/input.blade.php
+++ src/resources/views/input.blade.php
@@ -1,4 +1,4 @@
 <div @if(isset($cssid)) {{ $cssid }} @endif class="input content element @if(isset($cssclass)) {{ $cssclass }} @endif">
-	@if(isset($label))<label for={{ $name }}>{!! trans($label) !!}</label>@endif
-	<input name="{{ $name }}" type="{{ $type }}"<?php if(isset($html_attributes)): foreach($html_attributes as $attr => $val): if($attr != 'value'): ?> {{ $attr }}='{{ $val }}'<?php endif; endforeach; endif; if((isset($cache)) OR (isset($html_attributes->value))): ?> value='<?php if(isset($cache)): ?>{{ trans($cache) }}<?php else: ?>{{ trans($html_attributes->value) }}<?php endif; ?>'<?php endif; ?>>
+	@if(isset($label))<label for="{{ $name }}" class="ui label">{!! trans($label) !!}</label>@endif
+	<input name="{{ $name }}" type="{{ $type }}"<?php if(isset($html_attributes)): foreach($html_attributes as $attr => $val): if($attr != 'value'): ?> {{ $attr }}='{{ $val }}'<?php endif; endforeach; endif; ?> value='<?php if((isset($cache)) OR (isset($html_attributes->value))): ?> value='<?php if(isset($cache)): ?>{{ trans($cache) }}<?php else: ?>{{ trans($html_attributes->value) }}<?php endif; endif; ?>'>
 </div>
