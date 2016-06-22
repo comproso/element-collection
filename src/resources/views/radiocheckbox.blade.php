814ee36001be493566f163df1d40b63179ec2233
Thiemo Kunze <kunze@wangaz.com>
2016-06-22 11:51:10 +0200

    adding semantic ui to labels

--- src/resources/views/radiocheckbox.blade.php
+++ src/resources/views/radiocheckbox.blade.php
@@ -1,7 +1,7 @@
 <div<?php if(isset($cssid)): ?> id="{{ $cssid }}"<?php endif; ?> class="{{ $type }} content element<?php if(isset($cssclass)): ?> {{ $cssclass }}<?php endif; ?>">
 	<?php if(isset($label)): ?><label>{!! trans($label) !!}</label><?php endif; ?>
 	<?php foreach($options as $lbl => $val): ?>
-		<?php if(!is_int($lbl)): ?> <label for="{{ $name }}">{!! trans($lbl) !!}</label><?php endif; ?>
+		<?php if(!is_int($lbl)): ?> <label for="{{ $name }}" class="ui label">{!! trans($lbl) !!}</label><?php endif; ?>
 		<input name="{{ $name }}" type="{{ $type }}"<?php if($type != 'checkbox'): ?> value="{{ $val }}"<?php endif; if($val == $value): ?> checked="checked"<?php endif; ?>>
 	<?php endforeach; ?>
 </div>
